3f5441774141c123768cb6f11abea1fe28290148
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-28 17:31:55 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,13 +9,13 @@ end
 # code your input_to_index and move method here!
 def input_to_index(input)
   input = "1, 2, 3, 4, 5, 6, 7, 8, 9"
-'5'.to_i
+
 end
 board = [" ", " ", " "]
 def update_array_at_with(array, index, value)
   array[index] = value
 end
  def move(array, index, character = "X")
-   
+   '5'.to_i
  end
 update_array_at_with(board, 0, "X")
