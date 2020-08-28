794ebcfc82ea6c1e09eda0f2b5d59a4d2b5496cd
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-28 18:05:24 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,5 +12,9 @@ def input_to_index(input)
   input_to_i
 end
 def move (board, index, character = "X")
-
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+ 
+update_array_at_with(board, 0, "X")
 end
